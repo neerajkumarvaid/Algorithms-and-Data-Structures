4d7ea0b0fe0ae92cc39eecb964447c156f9b8073
Neeraj Kumar Vaid <neeraj.kumar.iitg@gmail.com>
2019-06-03 19:36:50 -0400

    Add files via upload
    This function recursively finds a peak in an array by repeatedly dividing the array into 2 sub-arrays until a peak is found.

---
+++ 1_peakfinding_1D.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 19:13:45 2019
+
+@author: Neeraj
+Lecture 1 : Peak finding algorithm for 1D
+
+"""
+
+def peak1d(array):
+    '''This function recursively finds a peak in an array by 
+    repeatedly dividing the array into 2 sub-arrays until 
+    a peak is found.
+    Complexity: O(log n)'''
+    start, stop = 0, len(array) # bounds for binary search
+    mid = (start + stop) // 2 # middle element 
+    
+    if mid >0 and array[mid+1] > array[mid] and array[mid] < array[mid-1]:
+        if array[mid+1] > array[mid-1]:
+            start = mid + 1
+            #print start, array[start:]
+            return peak1d2(array[start:])
+        else:
+            stop = mid-1
+            #print stop, array[:stop+1]
+            return peak1d2(array[:stop+1])
+    elif mid >0 and array[mid+1] > array[mid]:
+        start = mid + 1
+        return peak1d2(array[:stop+1])
+    elif mid >0 and array[mid+1] > array[mid]:
+        stop = mid-1
+        return peak1d2(array[start:])
+    else:
+        return array[mid]
+        
+# Run an example of 1D peak finding algorith,         
+array = [1, 3, 4, 3, 5, 1, 3]
+
+print peak1d(array)
